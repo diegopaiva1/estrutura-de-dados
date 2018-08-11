c37a2c313a4e856d4e200dd936ee3ca731df4781
Diego Paiva <diego.paiva13@gmail.com>
2018-08-11 13:37:08 -0300

    Testes em main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,10 @@
 
 int main(int argc, char const *argv[])
 {
-  int numbers[6] = {123, 984, 155, 30, 20, 200};
+  int numbers[15] = {15, 20, 98, 1, 50, 66, 25, 2059, 10580, 505, 1120, 505, 134, 988, 650};
   InsertionSort *insertionSort = new InsertionSort();
-  insertionSort->sort(numbers, 6);
-  for (int i = 0; i < 6; i++)
+  insertionSort->sort(numbers, 15);
+  for (int i = 0; i < 15; i++)
   {
     std::cout << numbers[i] << " ";
   }
