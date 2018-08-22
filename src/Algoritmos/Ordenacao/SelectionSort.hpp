7710b6009bf16057038d0934f6ce433d1e210e44
Diego Paiva <diego.paiva13@gmail.com>
2018-08-22 17:20:56 -0300

    Generalização do Selection Sort

--- src/Algoritmos/Ordenacao/SelectionSort.hpp
+++ src/Algoritmos/Ordenacao/SelectionSort.hpp
@@ -4,7 +4,7 @@
  * @date    11/08/2018
  *
  * O algoritmo SelectionSort é um algoritmo O(n²) instável para ordenação de elementos de uma lista.
- * Esta classe implementa o algoritmo para ordenação de um array de inteiros.
+ * Esta classe implementa o algoritmo para ordenação de um array de elementos de qualquer tipo.
  * A estratégia consiste em:
  * 1. Supor a existência de duas partições no array: a partição ordenada (inicialmente vazia)
  *    e a partição não-ordenada;
@@ -18,15 +18,32 @@
 #define SELECTIONSORT_H_INCLUDED
 
 #include <iostream>
-
-#include "../../../Utils/ArrayUtils/ArrayUtils.h"
+#include <vector>
+#include <algorithm> // std::distance, std::swap
 
 class SelectionSort
 {
 public:
   SelectionSort() {};
   ~SelectionSort() {};
-  void sort(int unsortedNumbers[], int size);
+
+  template<typename T>
+  std::vector<T> sort(std::vector<T> &unsorted)
+  {
+    for (unsigned int i = 0; i < unsorted.size(); i++)
+    {
+      int minElementIndex = getMinElementIndex(unsorted, i);
+      std::swap(unsorted[i], unsorted[minElementIndex]);
+    }
+    return unsorted;
+  }
+
+private:
+  template<typename T>
+  int getMinElementIndex(std::vector<T> v, int startingIndex = 0)
+  {
+    return std::distance(v.begin(), std::min_element(v.begin() + startingIndex, v.end()));
+  }
 };
 
 #endif // SELECTIONSORT_H_INCLUDED
--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,16 @@
 #include <string>
 #include <vector>
 
-#include "./Algoritmos/Ordenacao/SelectionSort/SelectionSort.h"
+#include "./Algoritmos/Ordenacao/SelectionSort.hpp"
 
 int main(int argc, char const *argv[])
 {
-  int numbers[15] = {3, 44, 12, 125, 98, 765, 12, 155, 99, 258, 13, 50, 49, 80, 450};
+  std::vector<int> v = {3, 44, 12, 125, 98, 765, 12, 155, 99, 258, 13, 50, 49, 80, 450};
   SelectionSort *selectionSort = new SelectionSort();
-  selectionSort->sort(numbers, 15);
-  for (int i = 0; i < 15; i++)
+  selectionSort->sort(v);
+  for (unsigned int i = 0; i < v.size(); i++)
   {
-    std::cout << numbers[i] << " ";
+    std::cout << v.at(i) << " ";
   }
   printf("\n");
   return 0;
