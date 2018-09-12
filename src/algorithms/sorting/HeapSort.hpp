175d5fab4781c89b6b7d02aabc7dc8db852dd204
Diego Paiva <diego.paiva13@gmail.com>
2018-09-11 23:14:08 -0300

    Métodos buildMaxHeap e maxHeapify

---
+++ src/algorithms/sorting/HeapSort.hpp
@@ -0,0 +1,67 @@
+/**
+ * @file    HeapSort.hpp
+ * @author  Diego Paiva e Silva
+ * @date    11/09/2018
+ *
+ */
+
+#ifndef HEAPSORT_H_INCLUDED
+#define HEAPSORT_H_INCLUDED
+
+#include <iostream>
+#include <vector>
+#include <algorithm> // std::swap
+
+class HeapSort
+{
+public:
+  HeapSort() {};
+  ~HeapSort() {};
+
+  void sort(std::vector<int> &unsorted)
+  {
+    buildMaxHeap(unsorted);
+    for(int i = unsorted.size() - 1; i >= 0; i--)
+    {
+      std::swap(unsorted.at(0), unsorted.at(i));
+      maxHeapify(unsorted, 0);
+    }
+  }
+
+private:
+  void buildMaxHeap(std::vector<int> &unsorted)
+  {
+    // Selecionamos o indíce do meio do vetor (ou próximo, caso tamanho seja ímpar)
+    int middleIndex = floor(unsorted.size()/2);
+
+    for (int i = middleIndex; i >= 0; i--)
+    {
+      maxHeapify(unsorted, i);
+    }
+  }
+
+  void maxHeapify(std::vector<int> &unsorted, unsigned int i)
+  {
+    unsigned int leftChildIndex  = 2 * i + 1;
+    unsigned int rightChildIndex = 2 * i + 2;
+    unsigned int maxIndex = i;
+
+    if(leftChildIndex < unsorted.size() && unsorted.at(leftChildIndex) > unsorted.at(i))
+    {
+      maxIndex = leftChildIndex;
+    }
+
+    if(rightChildIndex < unsorted.size() && unsorted.at(rightChildIndex) > unsorted.at(maxIndex))
+    {
+      maxIndex = rightChildIndex;
+    }
+
+    if(maxIndex != i)
+    {
+      std::swap(unsorted.at(i), unsorted.at(maxIndex));
+      maxHeapify(unsorted, maxIndex);
+    }
+  }
+};
+
+#endif // HEAPSORT_H_INCLUDED
--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,14 @@
-#include "components/file/reader/DeputyFileReader.hpp"
-#include "components/deputy/Deputy.hpp"
-#include "algorithms/sorting/InsertionSort.hpp"
+#include "algorithms/sorting/HeapSort.hpp"
 
 int main(int argc, char const *argv[])
 {
-  // Arquivo passado por linha de comando
-  std::string fileName = argv[1];
-
-  DeputyFileReader *deputyFileReader = new DeputyFileReader();
-
-  std::vector<Deputy> deputies = deputyFileReader->constructDeputies(fileName);
-
-  InsertionSort *mergeSort = new InsertionSort();
-  mergeSort->sort(deputies);
-
-  for (auto deputy : deputies)
+  std::vector<int> numbers = {2, 8, 5, 3, 9 , 1};
+  HeapSort *heapSort = new HeapSort();
+  heapSort->sort(numbers);
+  for(auto i : numbers)
   {
-    std::cout << "Nome: "                    << deputy.name              << std::endl;
-    std::cout << "Partido: "                 << deputy.party             << std::endl;
-    std::cout << "Nome do estabelecimento: " << deputy.establishmentName << std::endl;
-    std::cout << "Valor do recibo: "         << deputy.receiptValue      << std::endl;
-    printf("\n");
+    std::cout << i << " ";
   }
-
+  printf("\n");
   return 0;
 }
