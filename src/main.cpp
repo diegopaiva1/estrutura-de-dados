ba4695b99bc40636fa995b51c72f59e83dc8854d
Thaynara <thaynara@ice.ufjf.br>
2018-09-23 22:43:16 +0000

    main OpenAddressing

--- src/main.cpp
+++ src/main.cpp
@@ -4,50 +4,25 @@
 
 int main(int argc, char const *argv[])
 {
-  std::vector<int> data = {0,0,10,3,4};
+  std::vector<int> data = {1, 2, 10, 3, 4, 687, 93, 11};
 
-  HashTableQuadraticProbing *hashTableQuadraticProbing = new HashTableQuadraticProbing(data, 0.75);
-  HashTableLinearProbing *hashTableLinearProbing = new HashTableLinearProbing(data, 0.75);
-  HashTableDoubleHashing *hashTableDoubleHashing = new HashTableDoubleHashing(data, 0.75);
+  //HashTableQuadraticProbing *hashTableQuadraticProbing = new HashTableQuadraticProbing(data, 0.75);
+  HashTableOpenAddressing *hashTableOpenAddressing = new HashTableOpenAddressing(10, "Linear Probing");
+  //HashTableDoubleHashing *hashTableDoubleHashing = new HashTableDoubleHashing(data, 0.75);
 
-  hashTableQuadraticProbing->imprimir();
-  hashTableLinearProbing->imprimir();
-  hashTableDoubleHashing->imprimir();
+  hashTableOpenAddressing->insert(3);
 
-  hashTableQuadraticProbing->remove(165);
-  hashTableLinearProbing->remove(165);
-  hashTableDoubleHashing->remove(165);
+  //hashTableQuadraticProbing->imprimir();
+  //hashTableLinearProbing->imprimir();
+  //hashTableDoubleHashing->imprimir();
 
-  hashTableQuadraticProbing->imprimir();
-  hashTableLinearProbing->imprimir();
-  hashTableDoubleHashing->imprimir();
+  //hashTableQuadraticProbing->remove(3);
+  //hashTableLinearProbing->remove(3);
+  //hashTableDoubleHashing->remove(3);
 
-  try
-  {
-    std::cout << hashTableQuadraticProbing->get(165) << std::endl;
-  }
-  catch (const char* error)
-  {
-    std::cerr << error << '\n';
-  }
-
-  try
-  {
-    std::cout << hashTableLinearProbing->get(165) << std::endl;
-  }
-  catch (const char* error)
-  {
-    std::cerr << error << '\n';
-  }
-
-  try
-  {
-    std::cout << hashTableDoubleHashing->get(165) << std::endl;
-  }
-  catch (const char* error)
-  {
-    std::cerr << error << '\n';
-  }
+  //hashTableQuadraticProbing->imprimir();
+  //hashTableLinearProbing->imprimir();
+  //hashTableDoubleHashing->imprimir();
 
   return 0;
 }
