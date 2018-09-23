431ab7bd9feb909dab91c6cd9ee92ebe7cf6c548
Diego Paiva <diego.paiva13@gmail.com>
2018-09-23 19:48:57 -0300

    Rename da classe e exclusão das outras não-utilizadas

--- src/main.cpp
+++ src/main.cpp
@@ -6,23 +6,9 @@ int main(int argc, char const *argv[])
 {
   std::vector<int> data = {1, 2, 10, 3, 4, 687, 93, 11};
 
-  //HashTableQuadraticProbing *hashTableQuadraticProbing = new HashTableQuadraticProbing(data, 0.75);
   HashTableOpenAddressing *hashTableOpenAddressing = new HashTableOpenAddressing(10, "Linear Probing");
-  //HashTableDoubleHashing *hashTableDoubleHashing = new HashTableDoubleHashing(data, 0.75);
 
   hashTableOpenAddressing->insert(3);
-
-  //hashTableQuadraticProbing->imprimir();
-  //hashTableLinearProbing->imprimir();
-  //hashTableDoubleHashing->imprimir();
-
-  //hashTableQuadraticProbing->remove(3);
-  //hashTableLinearProbing->remove(3);
-  //hashTableDoubleHashing->remove(3);
-
-  //hashTableQuadraticProbing->imprimir();
-  //hashTableLinearProbing->imprimir();
-  //hashTableDoubleHashing->imprimir();
 
   return 0;
 }
