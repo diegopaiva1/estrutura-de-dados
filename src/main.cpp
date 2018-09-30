43acad52066c464b8ca23861113adeb43709d7b4
Diego Paiva <diego.paiva13@gmail.com>
2018-09-30 18:50:36 -0300

    Contabilizando métricas pro MergeSort

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 #include "components/SortingAlgorithmPerformanceMeasurer.hpp"
-#include "algorithms/sorting/SelectionSort.hpp"
+#include "algorithms/sorting/MergeSort.hpp"
 
 int main(int argc, char const *argv[])
 {
   std::string fileName = argv[1];
 
   SortingAlgorithmPerformanceMeasurer *measurer = new SortingAlgorithmPerformanceMeasurer();
-  measurer->storePerformanceResults(fileName, new SelectionSort());
+  measurer->storePerformanceResults(fileName, new MergeSort());
 
   return 0;
 }
