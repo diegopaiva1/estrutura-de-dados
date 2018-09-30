7a6b03c57101cd4551ffefa37ec24c927ac6b163
Diego Paiva <diego.paiva13@gmail.com>
2018-09-30 19:23:54 -0300

    Contabilizando métricas pro HeapSort

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 #include "components/SortingAlgorithmPerformanceMeasurer.hpp"
-#include "algorithms/sorting/MergeSort.hpp"
+#include "algorithms/sorting/HeapSort.hpp"
+
+#include <iomanip>
 
 int main(int argc, char const *argv[])
 {
   std::string fileName = argv[1];
 
   SortingAlgorithmPerformanceMeasurer *measurer = new SortingAlgorithmPerformanceMeasurer();
-  measurer->storePerformanceResults(fileName, new MergeSort());
+  measurer->storePerformanceResults(fileName, new HeapSort());
 
   return 0;
 }
