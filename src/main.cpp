8c2bcddfe51c70ecf52a8f77e12aaa4db569beff
Diego Paiva <diego.paiva13@gmail.com>
2018-09-30 19:37:22 -0300

    Remove <iomanip> main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,12 @@
 #include "components/SortingAlgorithmPerformanceMeasurer.hpp"
-#include "algorithms/sorting/HeapSort.hpp"
-
-#include <iomanip>
+#include "algorithms/sorting/MergeSort.hpp"
 
 int main(int argc, char const *argv[])
 {
   std::string fileName = argv[1];
 
   SortingAlgorithmPerformanceMeasurer *measurer = new SortingAlgorithmPerformanceMeasurer();
-  measurer->storePerformanceResults(fileName, new HeapSort());
+  measurer->storePerformanceResults(fileName, new MergeSort());
 
   return 0;
 }
