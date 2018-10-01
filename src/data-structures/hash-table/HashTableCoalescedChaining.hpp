2d7f7e0489ee0f79eef5f1d5621ebdb92572f69e
Diego Paiva <diego.paiva13@gmail.com>
2018-10-01 14:48:03 -0300

    Testes Hash coalescido

---
+++ src/data-structures/hash-table/HashTableCoalescedChaining.hpp
@@ -0,0 +1,133 @@
+#ifndef HASHTABLECOALESCEDCHAINING_H_INCLUDED
+#define HASHTABLECOALESCEDCHAINING_H_INCLUDED
+
+#include <iostream>
+#include <vector>
+
+class HashTableCoalescedChaining
+{
+public:
+  HashTableCoalescedChaining() {};
+  ~HashTableCoalescedChaining() {};
+
+  HashTableCoalescedChaining(std::vector<int> data, float loadFactor)
+  {
+    // Calculamos o tamanho da tabela
+    this->size = (int) data.size()/loadFactor;
+
+    // Alocamos espaço suficiente para os dados de entrada
+    this->data.resize(this->size);
+    this->pointer.resize(this->size);
+
+    for (unsigned int i = 0; i < data.size(); i++)
+    {
+      insert(data.at(i));
+    }
+  }
+
+  HashTableCoalescedChaining(int size)
+  {
+    // Tamanho da tabela
+    this->size = size;
+
+    // Alocamos espaço suficiente para os dados de entrada
+    this->data.resize(this->size);
+    this->pointer.resize(this->size);
+  }
+
+  void insert(int data)
+  {
+    int position = hash(data);
+    int collisions = 0;
+
+    while (this->data.at(position) != NULL && collisions != this->size)
+    {
+      position = (this->size - 1) - collisions;
+      collisions++;
+    }
+
+    if (this->data.at(position) == NULL)
+    {
+      this->data.at(position) = data;
+
+      if (collisions != 0)
+      {
+        int pointerPosition = hash(data);
+
+        while (this->pointer.at(pointerPosition) != NULL)
+        {
+          pointerPosition = this->pointer.at(pointerPosition);
+        }
+
+        if (this->pointer.at(pointerPosition) == NULL)
+        {
+          this->pointer.at(pointerPosition) = position;
+        }
+      }
+    }
+    else
+    {
+      throw "Tabela lotada!";
+    }
+  }
+
+  void remove(int data)
+  {
+    int position = hash(data);
+    int previousPosition;
+    int collisions = 0;
+
+    while (this->data.at(position) != data && collisions != this->size)
+    {
+      collisions++;
+      previousPosition = position;
+      position = this->pointer.at(position);
+    }
+
+    if (this->data.at(position) == data)
+    {
+      if (collisions != 0)
+      {
+        this->pointer.at(previousPosition) = this->pointer.at(position);
+      }
+      this->data.at(position) = NULL;
+      this->pointer.at(position) = NULL;
+    }
+    else
+    {
+      throw "Dado inexistente!";
+    }
+  }
+
+  int get(int data)
+  {
+    int position = hash(data);
+    int collisions = 0;
+
+    while (this->data.at(position) != data && collisions != this->size)
+    {
+      collisions++;
+      position = this->pointer.at(position);
+    }
+
+    if (this->data.at(position) == data)
+    {
+      return this->data.at(position);
+    }
+
+    throw "Dado inexistente!";
+  }
+
+private:
+  std::vector<int> data;
+  std::vector<int> pointer;
+  int size;
+
+  int hash(int data)
+  {
+    // Método da divisão
+    return data % this->size;
+  }
+};
+
+#endif // HASHTABLECOALESCEDCHAINING_H_INCLUDED
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,17 @@
 #include "components/SortingAlgorithmPerformanceMeasurer.hpp"
 #include "algorithms/sorting/HeapSort.hpp"
+#include "data-structures/hash-table/HashTableCoalescedChaining.hpp"
 
 int main(int argc, char const *argv[])
 {
   std::string fileName = argv[1];
 
-  SortingAlgorithmPerformanceMeasurer *measurer = new SortingAlgorithmPerformanceMeasurer();
-  measurer->storePerformanceResults(fileName, new HeapSort());
+  std::vector<int> data = {10, 20, 30, 40, 60, 110, 218, 191};
+
+  HashTableCoalescedChaining *t = new HashTableCoalescedChaining(data, 0.75);
+
+  // SortingAlgorithmPerformanceMeasurer *measurer = new SortingAlgorithmPerformanceMeasurer();
+  // measurer->storePerformanceResults(fileName, new HeapSort());
 
   return 0;
 }
