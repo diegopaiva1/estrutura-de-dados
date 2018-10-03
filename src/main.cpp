ac8850e125bd8c106494a1754215876b698d8974
Diego Paiva <diego.paiva13@gmail.com>
2018-10-03 12:42:31 -0300

    Testando alguns algoritmos no main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,19 @@
 #include "components/SortingAlgorithmPerformanceMeasurer.hpp"
 #include "algorithms/sorting/HeapSort.hpp"
-#include "data-structures/hash-table/HashTableCoalescedChaining.hpp"
+#include "algorithms/sorting/MergeSort.hpp"
+#include "algorithms/sorting/InsertionSort.hpp"
+#include "algorithms/sorting/SelectionSort.hpp"
 
 int main(int argc, char const *argv[])
 {
-  std::string fileName = argv[1];
+  // Arquivo passado por linha de comando
+  std::string inFile = argv[1];
 
-  std::vector<int> data = {10, 20, 30, 40, 60, 110, 218, 191};
-
-  HashTableCoalescedChaining *t = new HashTableCoalescedChaining(data, 0.75);
-
-  // SortingAlgorithmPerformanceMeasurer *measurer = new SortingAlgorithmPerformanceMeasurer();
-  // measurer->storePerformanceResults(fileName, new HeapSort());
+  SortingAlgorithmPerformanceMeasurer *measurer = new SortingAlgorithmPerformanceMeasurer();
+  measurer->storePerformanceResults(inFile, new HeapSort());
+  measurer->storePerformanceResults(inFile, new MergeSort());
+  measurer->storePerformanceResults(inFile, new InsertionSort());
+  measurer->storePerformanceResults(inFile, new SelectionSort());
 
   return 0;
 }
