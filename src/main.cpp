0a3c62d1bf00d0679fc614503f9470620b4428bc
Diego Paiva <diego.paiva13@gmail.com>
2018-11-01 12:51:50 -0300

    Imprimindo nós por nível para teste no main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,24 +7,29 @@ int main(int argc, char const *argv[])
 
   avlTree->root = avlTree->insert(1);
   avlTree->insert(2);
-  avlTree->insert(8);
-  avlTree->insert(10);
+  avlTree->insert(3);
+  avlTree->insert(4);
+  avlTree->insert(5);
+  avlTree->insert(6);
+  avlTree->insert(7);
+  avlTree->insert(15);
+  avlTree->insert(14);
+  avlTree->insert(13);
   avlTree->insert(12);
-  avlTree->insert(9);
+  avlTree->insert(11);
 
   try
   {
-    std::cout << "Raíz = " << avlTree->root->key << std::endl;
-    std::cout << "Fator de balanceamento da raíz = " << avlTree->root->balanceFactor() << std::endl;
-    std::cout << "Esquerda da raíz = " << avlTree->root->left->key << std::endl;
-    std::cout << "Fator de balanceamento da esquerda = " << avlTree->root->left->balanceFactor() << std::endl;
-    std::cout << "Direita da raíz = " << avlTree->root->right->key << std::endl;
-    std::cout << "Fator de balanceamento da direita = " << avlTree->root->right->balanceFactor() << std::endl;
-    std::cout << "Direita da direita = " << avlTree->root->right->right->key << std::endl;
-    std::cout << "Esquerda da direita = " << avlTree->root->right->left->key << std::endl;
-    std::cout << "Fator de balanceamento da direita da direita = " << avlTree->root->right->right->balanceFactor() << std::endl;
-    std::cout << "Fator de balanceamento da esquerda da direita = " << avlTree->root->right->left->balanceFactor() << std::endl;
-    std::cout << "Altura da árvore = " << avlTree->height() << std::endl;
+    std::cout << "Imprimindo nós por nível para teste" << '\n';
+    std::cout << avlTree->root->key << '\n';
+    std::cout << avlTree->root->left->key << " " << avlTree->root->right->key << '\n';
+    std::cout << avlTree->root->left->left->key << " " << avlTree->root->left->right->key << " "
+              << avlTree->root->right->left->key << " " << avlTree->root->right->right->key << '\n';
+    std::cout << avlTree->root->left->left->left->key << " "
+              << avlTree->root->left->left->right->key << " "
+              << avlTree->root->left->right->left->key << " "
+              << avlTree->root->right->left->left->key << " "
+              << avlTree->root->right->left->right->key << '\n';
   }
   catch (const char* exception)
   {
